680dc2074db14cec01e59362aa06aa45b0c287f1
ruhul <ra.01403288711@gmail.com>
2023-03-02 13:51:33 +0600

    Creating String

--- Introductory Problems/creating strings.cpp
+++ Introductory Problems/creating strings.cpp
@@ -1,37 +1,23 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-vector<string> generate_permutations(string s) {
-    vector<string> result;
-    if (s.size() == 1) {
-        result.push_back(s);
-        return result;
-    }
-    for (int i = 0; i < s.size(); i++) {
-    	
-        string sub = s.substr(0, i) + s.substr(i + 1);
-        cout << 0 << " " << i << " " << i+1 << endl;
-        cout << s.substr(0,i) << " " << s.substr(i+1) << endl;
-        vector<string> sub_perms = generate_permutations(sub);
-        for (string perm : sub_perms) {
-            result.push_back(s[i] + perm);
-        }
-    }
-    sort(result.begin(), result.end());
-    result.erase(unique(result.begin(), result.end()), result.end());
-    return result;
+void permutation(string str){
+	sort(str.begin(),str.end());
+	vector<string> perm;
+	do{
+		perm.push_back(str);
+	}while(next_permutation(str.begin(),str.end()));
+	cout << perm.size() << endl;
+	for(auto x:perm){
+		cout << x << endl;
+	}
 }
 
-int main() {
-    string s = "abc";
-    vector<string> perms = generate_permutations(s);
-    for (string perm : perms) {
-        cout << perm << endl;
-    }
-    return 0;
+int main(){
+	string str;
+	cin >> str;
+	permutation(str);
+	
+	
+	return 0;
 }
-
---
+++ String Algorithms/Reading Books.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+	vector<ll>num;
+	ll range;
+	cin >> range;
+	ll sum = 0;
+	for(ll i  = 0;i<range;i++){
+		ll x;cin >> x;
+		sum += x;
+		num.push_back(x);
+	}
+	sort(num.begin(),num.end());
+	ll high = num[range-1]*2;
+	cout << max(high,sum); 
+	return 0;
+}

268fdeff18553cdb2c6c2b00588a22b15524bda1
ruhul <ra.01403288711@gmail.com>
2023-03-26 16:32:31 +0600

    Static Range Sum Queries CSES

---
+++ Range Queries/Static Range Sum Queries.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main(){
+	ll range,query;
+	cin >> range >> query;
+	ll array[range];
+	ll sum = 0;
+	for(ll i = 0;i<range;i++){
+		ll x;cin >> x;
+		sum += x;
+		array[i] = sum;
+		//cout << sum << " ";
+	}
+	//cout << endl;
+	vector<pair<ll,ll>> qry;
+	vector<ll>ans;
+	for(ll i = 1;i<=query;i++){
+		ll x,y;
+		cin >> x >> y;
+		qry.push_back(make_pair(x,y));
+	}
+	for(ll i = 0;i<query;i++){
+		pair<ll,ll> temp = qry[i];
+		if(temp.first == 1)cout << array[temp.second-1] << endl;
+		else cout << array[temp.second-1] - array[temp.first-2] << endl;
+	}
+	
+	
+	return 0;
+}
